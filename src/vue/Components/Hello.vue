7078e759323bbeb60443da7495261e2d378d79ad
c.yamawaki <yamawaki00@gmail.com>
2020-09-11 20:41:08 +0900

    modify: index page

--- src/vue/Components/Hello.vue
+++ src/vue/Components/Hello.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
+  <div class="l-wrap">
     <h2 class="c-heading">{{ msg }}</h2>
+    <ul class="c-list">
+      <li class="c-list__child"
+        v-for="(list, key) in listItem"
+        :key="key"
+        >List{{list}}</li>
+    </ul>
   </div>
 </template>
 
@@ -9,7 +15,8 @@
     name: 'Hello',
     data () {
       return {
-        msg: 'Hello'
+        msg: 'Hello',
+        listItem: [1, 2, 3, 4, 5]
       }
     },
   }
@@ -17,8 +24,21 @@
 
 <style lang="scss" scoped>
   .c-heading {
+    margin-bottom: 40px;
     font-size: 2.6rem;
     font-weight: bold;
     letter-spacing: 0.15em;
+  }
+
+  .c-list {
+    list-style: '🐱';
+    padding-left: 20px;
+
+    &__child {
+      margin-bottom: 20px;
+      &:last-of-type {
+        margin-bottom: 0;
+      }
+    }
   }
 </style>
