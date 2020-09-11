7b89f08b0e20dffc9cc9cfac2bb00c1db8bd1eb3
c.yamawaki <yamawaki00@gmail.com>
2020-09-11 22:36:44 +0900

    modify: ダミーのテストデータで非同期テスト

--- src/scripts/__test__/_Hello.test.js
+++ src/scripts/__test__/_Hello.test.js
@@ -1,10 +1,18 @@
 /**
  * @file _Hello.test
  */
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import axios from 'axios';
+import flushPromises from 'flush-promises';
 import Hello from '@/vue/Components/Hello';
+import Api from '@/scripts/mixins/_api';
+jest.mock('axios')
 
-const wrapper = shallowMount(Hello);
+const localVue = createLocalVue();
+localVue.mixin(Api);
+
+const testListData = [1, 2, 3, 4, 5];
+const API_URL = '/assets/data/dammy.json';
 
 describe('Hello Component', () => {
   
@@ -14,7 +22,21 @@ describe('Hello Component', () => {
     expect(defaultData.msg).toBe('Hello');
   });
 
-  it('snapshot', () => {
+  test('get data', () => {
+    axios.get.mockImplementation((url) => {
+      if ( url === API_URL ) {
+        return Promise.resolve({ data: { list: testListData }})
+      }
+    })
+  })
+
+  test('snapshot', async () => {
+    const wrapper = shallowMount(Hello, {
+      localVue,
+      sync: false
+    });
+  
+    await flushPromises();
     expect(wrapper.element).toMatchSnapshot();
   });
 });
