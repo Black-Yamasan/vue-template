7719662ca485f164698d72e38c1cc46747f36372
Yama <yamawaki00@gmail.com>
2021-03-02 23:54:46 +0900

    fix: test code

--- src/scripts/__test__/_Hello.test.js
+++ src/scripts/__test__/_Hello.test.js
@@ -1,15 +1,11 @@
 /**
  * @file _Hello.test
  */
-import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import axios from 'axios';
 import flushPromises from 'flush-promises';
 import Hello from '@/vue/Components/Hello';
-import Api from '@/scripts/mixins/_api';
 jest.mock('axios')
-
-const localVue = createLocalVue();
-localVue.mixin(Api);
 
 const testListData = [1, 2, 3, 4, 5];
 const API_URL = '/assets/data/dammy.json';
@@ -32,7 +28,6 @@ describe('Hello Component', () => {
 
   test('snapshot', async () => {
     const wrapper = shallowMount(Hello, {
-      localVue,
       sync: false
     });
   
